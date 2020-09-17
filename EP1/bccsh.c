af096a1e862d10ae3644fedbbb0caa85b15472cc
kwy95 <rugone1000@hotmail.com>
2020-09-17 16:26:50 -0300

    updates

--- EP1/bccsh.c
+++ EP1/bccsh.c
@@ -2,13 +2,23 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/types.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
 void type_prompt();
 void read_command(char* cmd, char* args) {
-    scanf("test", cmd);
+    char* buf;
+
+    buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        // strcpy(str, buf);
+    }
     // cmd = (char*) malloc();
 }
 
