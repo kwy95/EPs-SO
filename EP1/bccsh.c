49d5c28586122e0d9e5d323b745b1ba9427caea5
kwy95 <rugone1000@hotmail.com>
2020-09-23 10:50:37 -0300

    algumas melhorias, mas nao muito

--- EP1/bccsh.c
+++ EP1/bccsh.c
@@ -11,31 +11,40 @@
 #include <readline/history.h>
 
 void type_prompt();
-void read_command(char* cmd, char* args) {
+void read_command(const char* prompt, char** cmd) {
     char* buf;
 
-    buf = readline("\n>>> ");
+    buf = readline(prompt);
     if (strlen(buf) != 0) {
         add_history(buf);
-        // strcpy(str, buf);
+        // strcpy(cmd, buf);
+        // cmd = &buf;
     }
-    // cmd = (char*) malloc();
+    // free(buf);
 }
 
 int main(int argc, char const **argv) {
-    char* command;
-    char* parameters;
+    char** command;
+    // char* parameters;
+    char* username = getenv("USER");
+    char* cwd = getcwd(NULL, 0);
+    int n = 0;
+
+    printf("user: %ld | %s\ncwd: %ld | %s\n", strlen(username), username, strlen(cwd), cwd);
+
     while (1) {
         //type_prompt();
-        read_command(command, parameters);
-        printf("%s", command);
-        if (fork() != 0) {
+        read_command("certinho>> ", command);
+        // printf("\n%s\n", *command);
+        // if (fork() != 0) {
             /* Codigo do pai */
-        } else {
+        // } else {
             /* Codigo do filho */
             // execve(command,parameters,0);
-        }
+        // }
+        // n++;
     }
+    // free(command);
 
     return 0;
 }
