a1febed0b937d88c41e3d13d57e160f5bef5a259
Murilo <murilo.felipe@radsquare.ai>
2020-10-03 16:06:03 -0300

    Read tracerout and create struct array

--- EP1/ep1.c
+++ EP1/ep1.c
@@ -1,3 +1,12 @@
+typedef struct trace
+{
+    char * nome;
+    int to;
+    int dt;
+    int deadline;
+} Trace;
+
+
 
 #include "util.h"
 
@@ -8,19 +17,28 @@ int main(int argc, char const **argv) {
     size_t len = 0;
     size_t read;
     char * pch;
+    int num_lines = 0;
+    int line_i = 0;
 
     fp = fopen(argv[1], "r");
-    printf("%s\n",argv[1] );
     if (fp == NULL)
         exit(EXIT_FAILURE);
+    while ((read = getline(&line, &len, fp)) != -1) {
+        num_lines++;
+    }
+
+    fclose(fp);
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    Trace traceroute[num_lines];
 
     while ((read = getline(&line, &len, fp)) != -1) {
-        pch = strtok(line, " ");
-        while (pch != NULL)
-        {
-            printf ("%s\n",pch);
-            pch = strtok (NULL, " ");
-        }
+        traceroute[line_i].nome = strtok(line, " ");
+        traceroute[line_i].to = atoi(strtok (NULL, " "));
+        traceroute[line_i].dt = atoi(strtok (NULL, " "));
+        traceroute[line_i].deadline = atoi(strtok (NULL, " "));
+        line_i++;
     }
 
     fclose(fp);
